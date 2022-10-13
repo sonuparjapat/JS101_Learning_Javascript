6dbed6608e142d7fa84376bf8f4d732a093a4bf1
sonuparjapat <sahil992001parjapat@gmail.com>
2022-10-13 12:40:22 +0530

    Fourth Commit

---
+++ day1/assignment4/index.js
@@ -0,0 +1,6 @@
+let Name="Name-Sonu";
+console.log(Name);
+let fathername="Father's Name-Hari Singh";
+console.log(fathername);
+let mothername="Mother's Name-Sudesh Kumari";
+console.log(mothername);
