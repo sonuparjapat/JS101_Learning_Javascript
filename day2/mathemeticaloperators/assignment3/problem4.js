e9e96fff70c26f97bcf941d06df612d564113686
sonuparjapat <sahil992001parjapat@gmail.com>
2022-10-13 13:14:13 +0530

    third commit

---
+++ day2/mathemeticaloperators/assignment3/problem4.js
@@ -0,0 +1,9 @@
+let stored_username="parjapat123";
+let stored_password="sk@34";
+//data put up by user
+let user_name="parjapat@123";
+let user_password="sk@123";
+if (user_name==stored_username)
+{if (user_password==stored_password){console.log("user can login");}
+else {console.log("check details");}}
+else {console.log("wrong credentials");}
